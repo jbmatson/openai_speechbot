c1eda60fa01c42117e51d6ce04f525e3cf2abb36
James Matson <james.matson@gmail.com>
2023-11-11 18:47:32 +1100

    Added call to ChatGPT to get a response and text to speech output

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # openai_speechbot
 
-Prerequisites:
+### Prerequisites:
 The following python packages must be installed:
 * pyaudio
 * openai
 * webrtcvad  (may require the Microsoft C++ Build Tools to be installed for Windows computers )
 * openai
+* elevenlabs
 
+*mpv* must be installed for ElevenLabs streaming audio output to work. Install from here: https://mpv.io/
+On Windows you may need to add the install folder to the path for the ElevenLabs library to be able to find it.
--- speechbot.py
+++ speechbot.py
@@ -5,16 +5,23 @@ import webrtcvad
 import os
 import json
 from openai import OpenAI
+from elevenlabs import generate, stream, set_api_key
 
 # A recommended way to store the key is in an environment variable. For this demo, I am storing in a file.
-key_location = 'C:\speechbot\openai_apikey.txt'
+openai_key_location = 'C:\speechbot\openai_apikey.txt'
 
-with open(key_location, 'r') as file:
-   api_key = file.readline().strip()
+with open(openai_key_location, 'r') as file:
+   openai_api_key = file.readline().strip()
 
 openai = OpenAI(
-   api_key = api_key
+   api_key = openai_api_key
 )
+
+elevenlabs_key_location = 'C:\speechbot\elevenlabs_apikey.txt'
+with open(elevenlabs_key_location, 'r') as file:
+   elevenlabs_api_key = file.readline().strip()
+
+set_api_key(elevenlabs_api_key)
 
 # Initialize the VAD (Voice Activity Detector)
 vad = webrtcvad.Vad()
@@ -29,13 +36,17 @@ RECORD_SECONDS = 10  # Maximum length of each audio segment
 WAVE_OUTPUT_FILENAME = "audio.wav"
 SILENCE_LIMIT = 2    # Number of seconds of silence/non-speech before we assume user has stopped talking
 
-# WebRTC VAD only accepts 16-bit mono PCM audio, sampled at 8000, 16000, 32000 or 48000 Hz. A frame must be either 10, 20, or 30 ms in duration.
+
+# NOTE: WebRTC VAD only accepts 16-bit mono PCM audio, sampled at 8000, 16000, 32000 or 48000 Hz. A frame must be either 10, 20, or 30 ms in duration.
 # For example, if your sample rate is 16000 Hz, then the only allowed frame/chunk sizes are 16000 * ({10,20,30} / 1000) = 160, 320 or 480 samples. 
 # Since each sample is 2 bytes (16 bits), the only allowed frame/chunk sizes are 320, 640, or 960 bytes.
 
+# Configure GPT parameters
+GPT_MODEL = "gpt-3.5-turbo"
+
 # Create a PyAudio stream for recording
 audio = pyaudio.PyAudio()
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+rec_stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
 # Initialize variables for audio data
 frames = []
@@ -43,10 +54,23 @@ audio_data = b""
 in_speech = False
 silence_threshold = 0
 
-print("Listening... (Press Ctrl+C to stop)")
-while True:
+chat_messages = []
+
+said_goodbye = False
+
+chat_messages.append({"role": "assistant", "content": "Hello, how can I help you?"}) 
+print("Assistant: Hello, how can I help you?")
+audio_stream = generate(
+   text="Hello, how can I help you?",
+   stream=True
+)
+
+stream(audio_stream)
+
+print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
+while True and not said_goodbye:
    try:
-      audio_chunk = stream.read(CHUNK)
+      audio_chunk = rec_stream.read(CHUNK)
       frames.append(audio_chunk)
       audio_data += audio_chunk
 
@@ -104,15 +128,38 @@ while True:
                   
                audio_data = b""
 
-               # TODO: Send the transcription text to ChatGPT to get a response
+               # Send the transcription text to ChatGPT to get a response
 
-               # TODO: Send the response to a text-to-speect service, stream the response to audio output
+               chat_messages.append({ "role": "user", "content": transcription.text })
 
+               response = openai.chat.completions.create(
+                  model = GPT_MODEL,
+                  messages = chat_messages,
+                  temperature = 0.7
+               )
 
+               response_text = response.choices[0].message.content
+               print("Assistant: " + response_text)
+               chat_messages.append({"role": "assistant", "content": response_text}) 
+
+               # Send the response to ElevenLabs text-to-speect service, stream the response to audio output
+
+               audio_stream = generate(
+                  text=response_text,
+                  stream=True
+               )
+
+               stream(audio_stream)
+
+               if transcription.text.lower() == "goodbye.":
+                  said_goodbye = True
+               else:
+                  print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
+  
    except KeyboardInterrupt:
       break
 
 # Cleanup
-stream.stop_stream()
-stream.close()
+rec_stream.stop_stream()
+rec_stream.close()
 audio.terminate()
