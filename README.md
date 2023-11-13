e89cac921412f48950d9a6b0883a645c4d907a1e
James Matson <james.matson@gmail.com>
2023-11-13 20:04:57 +1100

    Refactored code and added more comments

--- README.md
+++ README.md
@@ -9,4 +9,10 @@ The following python packages must be installed:
 * elevenlabs
 
 *mpv* must be installed for ElevenLabs streaming audio output to work. Install from here: https://mpv.io/
+
 On Windows you may need to add the install folder to the path for the ElevenLabs library to be able to find it.
+
+### Ideas for future projects
+
+* Call into a customised VoiceFlow flow instead of using ChatGPT  - make a simple emergency services call operator flow to use
+* Instead of using a Python script, build it as a simple Django website. Would need to investigate if/how it would be possible to do the voice recording and voice activity detection in the web client (WebRTC VAD?), stream the voice recording to the server, then stream the voice response back to the client
--- speechbot.py
+++ speechbot.py
@@ -1,165 +1,211 @@
-import pyaudio
+"""
+This is a simple chat bot with that uses webrtvad for voice recording, 
+OpenAI whisper transcription, ChatGPT, and Whisper.
+"""
 import wave
-import webrtcvad
-#mport requests
 import os
-import json
+import pyaudio
+import webrtcvad
 from openai import OpenAI
 from elevenlabs import generate, stream, set_api_key
 
-# A recommended way to store the key is in an environment variable. For this demo, I am storing in a file.
-openai_key_location = 'C:\speechbot\openai_apikey.txt'
-
-with open(openai_key_location, 'r') as file:
-   openai_api_key = file.readline().strip()
-
-openai = OpenAI(
-   api_key = openai_api_key
-)
-
-elevenlabs_key_location = 'C:\speechbot\elevenlabs_apikey.txt'
-with open(elevenlabs_key_location, 'r') as file:
-   elevenlabs_api_key = file.readline().strip()
-
-set_api_key(elevenlabs_api_key)
-
-# Initialize the VAD (Voice Activity Detector)
-vad = webrtcvad.Vad()
-vad.set_mode(3)  # You can experiment with different modes (1-3) for sensitivity
-
 # Configure the audio recording parameters
+
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000         # Samples per second
-CHUNK = 320          # chunk size
+CHUNK = 320          # chunk size in bytes
 RECORD_SECONDS = 10  # Maximum length of each audio segment
 WAVE_OUTPUT_FILENAME = "audio.wav"
-SILENCE_LIMIT = 2    # Number of seconds of silence/non-speech before we assume user has stopped talking
+SILENCE_LIMIT = 2     # Number of seconds of silence/non-speech before
+                      #   we assume user has stopped talking
 
-
-# NOTE: WebRTC VAD only accepts 16-bit mono PCM audio, sampled at 8000, 16000, 32000 or 48000 Hz. A frame must be either 10, 20, or 30 ms in duration.
-# For example, if your sample rate is 16000 Hz, then the only allowed frame/chunk sizes are 16000 * ({10,20,30} / 1000) = 160, 320 or 480 samples. 
-# Since each sample is 2 bytes (16 bits), the only allowed frame/chunk sizes are 320, 640, or 960 bytes.
+# NOTE: WebRTC VAD only accepts 16-bit mono PCM audio, sampled at 8000, 16000, 32000 or 48000 Hz.
+# A frame must be either 10, 20, or 30 ms in duration.
+# For example, if your sample rate is 16000 Hz, then the only allowed frame/chunk sizes are
+# 16000 * ({10,20,30} / 1000) = 160, 320 or 480 samples.
+# Since each sample is 2 bytes (16 bits), the only allowed frame/chunk sizes are
+# 320, 640, or 960 bytes.
 
 # Configure GPT parameters
 GPT_MODEL = "gpt-3.5-turbo"
 
-# Create a PyAudio stream for recording
-audio = pyaudio.PyAudio()
-rec_stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+chat_messages = []   # the chat message history
 
-# Initialize variables for audio data
-frames = []
-audio_data = b""
-in_speech = False
-silence_threshold = 0
+def main():
+    """
+    This is the main function for the SpeechBot script
+    """
 
-chat_messages = []
+    # Get API keys
+    # The recommended way to store the keys is in environment variables.
+    # For this demo, I am storing them in files.
 
-said_goodbye = False
+    # openai_key_location = 'C:\speechbot\openai_apikey.txt'
+    # with open(openai_key_location, 'r') as file:
+    #     openai_api_key = file.readline().strip()
+    openai_api_key = get_api_key_from_file('C:\\speechbot\\openai_apikey.txt')
+    openai = OpenAI(
+        api_key = openai_api_key
+    )
 
-chat_messages.append({"role": "assistant", "content": "Hello, how can I help you?"}) 
-print("Assistant: Hello, how can I help you?")
-audio_stream = generate(
-   text="Hello, how can I help you?",
-   stream=True
-)
+    # elevenlabs_key_location = 'C:\speechbot\elevenlabs_apikey.txt'
+    # with open(elevenlabs_key_location, 'r') as file:
+    #     elevenlabs_api_key = file.readline().strip()
+    elevenlabs_api_key = get_api_key_from_file('C:\\speechbot\\elevenlabs_apikey.txt')
+    set_api_key(elevenlabs_api_key)
 
-stream(audio_stream)
+    # Initialize the VAD (Voice Activity Detector)
+    vad = webrtcvad.Vad()
+    vad.set_mode(3)  # You can experiment with different modes (1-3) for sensitivity
 
-print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
-while True and not said_goodbye:
-   try:
-      audio_chunk = rec_stream.read(CHUNK)
-      frames.append(audio_chunk)
-      audio_data += audio_chunk
+    # Create a PyAudio stream for recording
+    audio = pyaudio.PyAudio()
+    rec_stream = audio.open(format=FORMAT,
+                            channels=CHANNELS,
+                            rate=RATE,
+                            input=True,
+                            frames_per_buffer=CHUNK)
 
-      is_speech = vad.is_speech(audio_chunk, RATE)
+    # Initialize variables for audio data
+    frames = []
+    audio_data = b""
+    in_speech = False
+    silence_threshold = 0
 
-      if is_speech:
-         if not in_speech:
-                  # if speech is detected but script not aware of speech
-                  # make it aware
-                  in_speech = True
-                  silence_threshold = 0
-         continue
-      elif not is_speech and in_speech:
-         # check if user has been quiet long enough to assume they have stopped talking
-         if silence_threshold < SILENCE_LIMIT * (RATE / CHUNK):
-            # not yet - wait longer
-            silence_threshold += 1
-            continue
-         else:
-            # silence has reached limit - process and send the recording for transcription
-            in_speech = False
-            if audio_data:
-               # Save the audio segment to a WAV file
-               with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
-                  wf.setnchannels(CHANNELS)
-                  wf.setsampwidth(audio.get_sample_size(FORMAT))
-                  wf.setframerate(RATE)
-                  wf.writeframes(audio_data)
-                  wf.close()
-                  
-               # Send the audio segment to OpenAI Whisper for speech recognition
-               audio_url = "https://api.openai.com/v1/audio/transcriptions"
-               audio_file = open(WAVE_OUTPUT_FILENAME, 'rb')
+    # Initialise variables for chat conversation
+    said_goodbye = False
 
-               try:
-                  transcription = openai.audio.transcriptions.create(
-                     model = "whisper-1", 
-                     file = audio_file
-                  )
-               except openai.APIError as e:
-                  print(f"OpenAI API returned an API Error: {e}")
-                  pass
-               except openai.APIConnectionError as e:
-                  print(f"Failed to connect to OpenAI API: {e}")
-                  pass
-               except openai.RateLimitError as e:
-                  print(f"OpenAI API request exceeded rate limit: {e}")
-                  pass
+    speak_string("assistant", "Hello, how can I help you?")
 
-               print("Transcription:", transcription.text)
-               audio_file.close()
-               
-               # Remove the temporary WAV file
-               os.remove(WAVE_OUTPUT_FILENAME)
-                  
-               audio_data = b""
+    print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
+    while True and not said_goodbye:
+        try:
+            audio_chunk = rec_stream.read(CHUNK)
+            frames.append(audio_chunk)
+            audio_data += audio_chunk
 
-               # Send the transcription text to ChatGPT to get a response
+            is_speech = vad.is_speech(audio_chunk, RATE)
 
-               chat_messages.append({ "role": "user", "content": transcription.text })
+            if is_speech:
+                if not in_speech:
+                    # if speech is detected but script not aware of speech
+                    # make it aware
+                    in_speech = True
+                    silence_threshold = 0
+                    continue
+            elif not is_speech and in_speech:
+                # check if user has been quiet long enough to assume they have stopped talking
+                if silence_threshold < SILENCE_LIMIT * (RATE / CHUNK):
+                    # not yet - wait longer
+                    silence_threshold += 1
+                    continue
+                else:
+                    # silence has reached limit - process and send the recording for transcription
+                    in_speech = False
+                if audio_data:
+                    # Save the audio segment to a WAV file
+                    with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
+                        wf.setnchannels(CHANNELS)
+                        wf.setsampwidth(audio.get_sample_size(FORMAT))
+                        wf.setframerate(RATE)
+                        wf.writeframes(audio_data)
+                        wf.close()
+                    
+                    # Send the audio segment to OpenAI Whisper for speech recognition
+                    audio_file = open(WAVE_OUTPUT_FILENAME, 'rb')
 
-               response = openai.chat.completions.create(
-                  model = GPT_MODEL,
-                  messages = chat_messages,
-                  temperature = 0.7
-               )
+                    try:
+                        transcription = openai.audio.transcriptions.create(
+                            model = "whisper-1",
+                            file = audio_file
+                        )
+                    except openai.OpenAIError as e:
+                        print(e.http_status)
+                        print(e.error)
 
-               response_text = response.choices[0].message.content
-               print("Assistant: " + response_text)
-               chat_messages.append({"role": "assistant", "content": response_text}) 
+                    print("Transcription:", transcription.text)
+                    audio_file.close()
 
-               # Send the response to ElevenLabs text-to-speect service, stream the response to audio output
+                    chat_messages.append({ "role": "user", "content": transcription.text })
 
-               audio_stream = generate(
-                  text=response_text,
-                  stream=True
-               )
+                    # Remove the temporary WAV file
+                    os.remove(WAVE_OUTPUT_FILENAME)
 
-               stream(audio_stream)
+                    audio_data = b""
 
-               if transcription.text.lower() == "goodbye.":
-                  said_goodbye = True
-               else:
-                  print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
-  
-   except KeyboardInterrupt:
-      break
+                    # Send the transcription text to ChatGPT to get a response
+                    response = openai.chat.completions.create(
+                        model = GPT_MODEL,
+                        messages = chat_messages,
+                        temperature = 0.7
+                    )
 
-# Cleanup
-rec_stream.stop_stream()
-rec_stream.close()
-audio.terminate()
+                    response_text = response.choices[0].message.content
+
+                    speak_string("assistant", response_text)
+                    # print("Assistant: " + response_text)
+                    # chat_messages.append({"role": "assistant", "content": response_text})
+
+                    # # Send the response to ElevenLabs text-to-speect service,
+                    # #  stream the response to audio output
+
+                    # audio_stream = generate(
+                    #    text=response_text,
+                    #    voice="Rachel",
+                    #    model="eleven_multilingual_v2",
+                    #    stream=True
+                    # )
+
+                    # stream(audio_stream)
+
+                    if transcription.text.lower() == "goodbye.":
+                        said_goodbye = True
+                    else:
+                        print("Listening... (Press Ctrl+C to stop or say 'goodbye')")
+
+        except KeyboardInterrupt:
+            break
+
+    # Cleanup
+    rec_stream.stop_stream()
+    rec_stream.close()
+    audio.terminate()
+
+
+def speak_string(role, text):
+    """
+    Speak a string using the ElevenLabs text-to-speech streaming API, 
+    adds it to the chat messages list, and prints to terminal for the given role.
+
+    :param role: The role of the speaker
+    :param text: The text to speak
+    """
+    chat_messages.append({"role": role, "content": text})
+
+    print(role + ": " + text)
+
+    audio_stream = generate(
+        text = text,
+        voice = "Rachel",
+        model = "eleven_multilingual_v2",
+        stream=True
+    )
+
+    stream(audio_stream)
+
+
+def get_api_key_from_file(key_file_path):
+    """
+    Gets an API key from the given file location
+
+    :param key_file_path: The path to the API key file
+    """
+    #key_location = 'C:\speechbot\elevenlabs_apikey.txt'
+    # TODO: Add handling for file not found or key can't be read
+    with open(key_file_path, 'r') as file:
+        api_key = file.readline().strip()
+
+    return api_key
+
+main()
